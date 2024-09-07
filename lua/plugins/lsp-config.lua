ecd08dd615ae8bafc5b206e11a70d93013ca4789
AsgerIngemann <asger.i.rasmussen@gmail.com>
2024-09-07 11:24:29 +0200

    config

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,33 @@
+return {
+	{
+		"williamboman/mason.nvim",
+			config = function()
+				require("mason").setup()
+			end
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+			config = function()
+				require("mason-lspconfig").setup({
+					ensure_installed = {"r_language_server", "pyright"}
+				})
+				end
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+			local lspconfig = require("lspconfig")
+			lspconfig.pyright.setup({
+        capabilities = capabilities 
+      })
+			lspconfig.r_language_server.setup({
+        capabilities = capabilities 
+      })
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+      vim.keymap.set({'n'}, '<leader>ca', vim.lsp.buf.code_action, {}) 
+		end
+	},
+}
+
---
+++ lua/vim-options.lua
@@ -0,0 +1,15 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set textwidth=70")
+
+
+
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+vim.o.number = true
+
+
+
